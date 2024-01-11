cbadaf064516059981e400c4ea8439bf9d97a1ff
Yuxuan Jiang <jiangy10@rose-hulman.edu>
2024-01-11 02:04:22 -0500

    body created

--- moving-eye/src/MovingEye.ts
+++ moving-eye/src/MovingEye.ts
@@ -1,59 +1,120 @@
 import {html, css, LitElement} from 'lit';
-import {customElement, state} from 'lit/decorators.js';
+import {customElement, property, state} from 'lit/decorators.js';
 import {animate} from '@lit-labs/motion';
 
 @customElement('moving-eye')
 export class MovingEye extends LitElement {
-  //displacement of eye position
-  @state() dx = 0;
-  @state() dy = 0;
+	//displacement of eye position
+	@state() dx: number = 0;
+	@state() dy: number = 0;
 
-  static styles = css`
-    #eye {
-      position: absolute;
-      left: 200px;
-      top: 200px;
-      background-color: black;
-      width: 20px;
-      height: 20px;
-      border-radius: 10px;
-      //transform: translate(-50%, -50%);
-    }`;
 
-  constructor() {
-    super();
-    //enable the mouse detection in entire webpage instead of this shadow dom
-    document.addEventListener('mousemove', event => this.moveEye(event));
-  }
+	static styles = css`
+      .eye {
+        position: absolute;
+        background-color: black;
+        width: 8px;
+        height: 8px;
+        border-radius: 5px;
+      }`;
 
-  //lifecycle: manually remove external event listener
-  disconnectedCallback() {
-    super.disconnectedCallback();
-    document.removeEventListener('mousemove', this.moveEye);
-  }
+	constructor() {
+		super();
+		//enable the mouse detection in entire webpage instead of this shadow dom
+		document.addEventListener('mousemove', event => this.moveEye(event));
+	}
 
-  render() {
-    return html`
-      <div id="eye" style="transform: translate(${this.dx}px, ${this.dy}px)" ></div>`;
-  }
+	//lifecycle: manually remove external event listener
+	disconnectedCallback() {
+		super.disconnectedCallback();
+		document.removeEventListener('mousemove', this.moveEye);
+	}
 
-  moveEye(event: MouseEvent) {
-    const pupil: Element | null | undefined = this.shadowRoot?.querySelector('#eye');
-    if (pupil) {
-      const pupilPosition = pupil.getBoundingClientRect();
-      const distance = Math.sqrt((event.clientX - pupilPosition.x) ** 2 + (event.clientY - pupilPosition.y) ** 2);
-      //look at mouse
-      if (distance > 50) {
-        this.dx = (event.clientX - pupilPosition.x) * (50 / distance);
-        this.dy = (event.clientY - pupilPosition.y) * (50 / distance);
+	render() {
+		return html`
+            <div class="eye"
+                 style="transform: translate(${this.dx}px, ${this.dy}px)">
+
+            </div>`;
+	}
+
+	moveEye(event: MouseEvent): void {
+		const eye: Element | null | undefined = this.shadowRoot?.querySelector('.eye');
+		if (eye) {
+			const eyePosition = eye.getBoundingClientRect();
+			const distance = Math.sqrt((event.clientX - eyePosition.x) ** 2 + (event.clientY - eyePosition.y) ** 2);
+			//look at mouse
+			if (distance > 5) {
+				this.dx = (event.clientX - eyePosition.x) * (8 / distance);
+				this.dy = (event.clientY - eyePosition.y) * (8 / distance);
+			}
+			//back to origin
+			else {
+				this.dx = 0;
+				this.dy = 0;
+			}
+		}
+	}
+}
+
+@customElement('semi-circle')
+export class SemiCircle extends LitElement {
+	//height adjustment
+	@state() dh: number = 0;
+
+	static styles = css`
+      #semicircle {
+        position: relative;
+        left: 200px;
+        top: 200px;
+        width: 150px;
+        height: 80px;
+        border-top-left-radius: 100px;
+        border-top-right-radius: 100px;
+        border-bottom-left-radius: 10px;
+        border-bottom-right-radius: 10px;
+        background-color: orange;
+        display: flex;
+        justify-content: center;
+        //transform: skewX(-10deg);
       }
-      //back to origin
-      else{
-        this.dx = 0;
-        this.dy = 0;
+
+      #face {
+        position: relative;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-evenly;
+        align-items: center;
+        width: 120px;
+        height: 60px;
       }
-    }
-  }
+	`;
 
+	constructor() {
+		super();
+		document.addEventListener('mousemove', event => this.moveBody(event));
+	}
 
+	render() {
+		return html`
+            <div id="semicircle">
+                <div id="face">
+                    <moving-eye></moving-eye>
+                    <moving-eye></moving-eye>
+                </div>
+            </div>`;
+	}
+
+	moveBody(event: MouseEvent): void {
+		const body: Element | null | undefined = this.shadowRoot?.querySelector('#semi-circle');
+		if (body) {
+			const bodyPosition = body.getBoundingClientRect();
+			//mouse is above the body
+			if (event.clientY < bodyPosition.y) {
+				this.dh = (bodyPosition.y - event.clientY) / 20;
+			}else{
+				this.dh = 0
+			}
+		}
+	}
 }
